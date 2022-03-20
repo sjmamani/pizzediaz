209c178233ab5b94e555658ef3dcfed7c71bdf9a
Santiago Mamani <santiagojmamani@gmail.com>
2022-03-20 19:05:13 -0300

    Update onboarding_screen.dart
    Update paddings

--- lib/src/presentation/screens/onboarding/onboarding_screen.dart
+++ lib/src/presentation/screens/onboarding/onboarding_screen.dart
@@ -27,79 +27,87 @@ class OnboardingScreen extends ConsumerWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.backgroundColor,
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(16, 60, 16, 10),
-          child: Column(
-            children: [
-              Expanded(
-                child: PageView(
-                  controller: pageController,
-                  onPageChanged: (value) =>
-                      ref.read(onboardingProvider.notifier).updateIndex(value),
-                  children: [
-                    _OnboardingPage(
-                      title: 'Pedí una pizza',
-                      textContent:
-                          'Elegí qué pizza vas a cenar esta noche y aboná de la manera que quieras',
-                      selectedIndex: selectedIndex,
-                      assetName: 'assets/images/onboarding_1.svg',
-                    ),
-                    _OnboardingPage(
-                      title: 'Esperá tu delivery',
-                      textContent:
-                          'Nuestros deliverys te lo alcanzarán sin costo de envío a tu domicilio',
-                      selectedIndex: selectedIndex,
-                      assetName: 'assets/images/onboarding_2.svg',
-                    ),
-                    _OnboardingPage(
-                      title: 'Disfrutá la comida',
-                      textContent: 'Disfrutá de nuestras pizzas en familia',
-                      selectedIndex: selectedIndex,
-                      assetName: 'assets/images/onboarding_3.svg',
-                    ),
-                  ],
-                ),
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                controller: pageController,
+                onPageChanged: (value) =>
+                    ref.read(onboardingProvider.notifier).updateIndex(value),
+                children: [
+                  _OnboardingPage(
+                    title: 'Pedí una pizza',
+                    textContent:
+                        'Elegí qué pizza vas a cenar esta noche y aboná de la manera que quieras',
+                    selectedIndex: selectedIndex,
+                    assetName: 'assets/images/onboarding_1.svg',
+                  ),
+                  _OnboardingPage(
+                    title: 'Esperá tu delivery',
+                    textContent:
+                        'Nuestros deliverys te lo alcanzarán sin costo de envío a tu domicilio',
+                    selectedIndex: selectedIndex,
+                    assetName: 'assets/images/onboarding_2.svg',
+                  ),
+                  _OnboardingPage(
+                    title: 'Disfrutá la comida',
+                    textContent: 'Disfrutá de nuestras pizzas en familia',
+                    selectedIndex: selectedIndex,
+                    assetName: 'assets/images/onboarding_3.svg',
+                  ),
+                ],
               ),
-              MainButton(
-                text: 'Siguiente',
-                onPressed: () {
-                  if (selectedIndex < 2) {
-                    pageController.nextPage(
-                      duration: const Duration(milliseconds: 400),
-                      curve: Curves.ease,
-                    );
-                    ref
-                        .read(onboardingProvider.notifier)
-                        .updateIndex(selectedIndex + 1);
-                  } else {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute<void>(
-                        builder: (BuildContext context) => const LoginScreen(),
-                      ),
-                    );
-                  }
-                },
-              ),
-              const SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(3, (index) {
-                  return Container(
-                    margin: const EdgeInsets.only(right: 8),
-                    height: 8,
-                    width: 8,
-                    decoration: BoxDecoration(
-                      color: index == selectedIndex
-                          ? AppColors.primaryColor
-                          : AppColors.primaryColor.withOpacity(0.25),
-                      shape: BoxShape.circle,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 12, 16, 10),
+              child: Column(
+                children: [
+                  MainButton(
+                    text: 'Siguiente',
+                    onPressed: () {
+                      if (selectedIndex < 2) {
+                        pageController.nextPage(
+                          duration: const Duration(milliseconds: 400),
+                          curve: Curves.ease,
+                        );
+                        ref
+                            .read(onboardingProvider.notifier)
+                            .updateIndex(selectedIndex + 1);
+                      } else {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) =>
+                                const LoginScreen(),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      3,
+                      (index) {
+                        return Container(
+                          margin: const EdgeInsets.only(right: 8),
+                          height: 8,
+                          width: 8,
+                          decoration: BoxDecoration(
+                            color: index == selectedIndex
+                                ? AppColors.primaryColor
+                                : AppColors.primaryColor.withOpacity(0.25),
+                            shape: BoxShape.circle,
+                          ),
+                        );
+                      },
                     ),
-                  );
-                }),
-              )
-            ],
-          ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -125,7 +133,10 @@ class _OnboardingPage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(title, style: AppTextStyles.h1),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 60, 16, 0),
+          child: Text(title, style: AppTextStyles.h1),
+        ),
         Expanded(child: SvgPicture.asset(assetName)),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 48),
